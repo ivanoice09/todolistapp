ee69a9fde3524b52873ba4c0a5eda4213e50b8bf
Ivan-da-casa <ivankovicsibsion@gmail.com>
2025-05-02 23:41:16 +0200

    website main pages

---
+++ calendar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendar</title>
+    <link href="css/fab.css" rel="stylesheet">
+    <script src="task_modal.js"></script>
+</head>
+
+<body>
+    <!-- sidemenu -->
+    <?php require 'sidemenu.php'; ?>
+
+    <!-- task modal -->
+    <?php require 'task_modal.html'; ?>
+</body>
+
+</html>
